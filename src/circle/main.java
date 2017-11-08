fd7e813a08657d8c32785b525bcd95baa4211123
Abbehh <abbejytgmail.com>
2017-11-08 13:58:26 +0100

    Jag började med min skit

---
+++ src/circle/main.java
@@ -0,0 +1,33 @@
+package circle;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+	public class main extends Application {
+		
+		public static double worldWidth = 500;
+		public static double worldHeight = 500;
+	
+		public void start(Stage stage) {
+			
+		Group root = new Group();
+		Scene scene = new Scene(root,worldWidth,worldHeight);
+		
+		
+		
+		
+		
+	}
+
+		private static void main(String[] args) {
+			launch(args);
+			
+		}
+	
+	
+	
+
+}
